d439efe94b726e09f99b2142340239a3b16563a5
Luan Vu <luanvv@techcombank.com.vn>
2021-06-08 00:35:58 +0700

    first commit

---
+++ bookstore/src/main/java/com/luanvv/microservices/bookstore/config/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.luanvv.microservices.bookstore.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal1(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable()
+            .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/books").permitAll()
+                .antMatchers(HttpMethod.GET, "/books/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/books").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/books/*").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/books/*").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+            .csrf()
+                .disable();
+    }
+}
