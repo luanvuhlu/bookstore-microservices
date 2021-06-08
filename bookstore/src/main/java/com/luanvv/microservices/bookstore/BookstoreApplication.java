fe12c1ce3c9f916a0bbd687f581b57a8b0ec34c1
Luan Vu <luanvv@techcombank.com.vn>
2021-06-08 22:35:31 +0700

    add configuration server and application config repo

--- bookstore/src/main/java/com/luanvv/microservices/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/luanvv/microservices/bookstore/BookstoreApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class Bookstore {
+public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
