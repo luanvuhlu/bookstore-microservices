eea36fd4e3556e348b78b23cb870a63c4100b2d4
luanvu <luanvuhlu@gmail.com>
2021-06-12 00:35:32 +0700

    add Spring gateway

---
+++ gateway/src/test/java/com/luanvv/microservices/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanvv.microservices.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
