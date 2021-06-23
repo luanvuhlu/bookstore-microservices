b51d10bce6b4d8a3f1982b7d2b4d8b2fc44b4dab
luanvu <luanvuhlu@gmail.com>
2021-06-23 22:55:18 +0700

    test jenkins

--- bookstore/src/main/java/com/luanvv/microservices/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/luanvv/microservices/bookstore/controller/BookController.java
@@ -3,13 +3,11 @@ package com.luanvv.microservices.bookstore.controller;
 import java.io.IOException;
 import java.util.Optional;
 import java.util.Random;
-import java.util.concurrent.Callable;
 
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,22 +30,6 @@ public class BookController {
 	public String getBooks() {
 		log.info("Get all books");
 		return " All books";
-	}
-	
-	@RequestMapping("/call")
-	public Callable<String> call() {
-		return new Callable<String>() {
-			@Override
-			public String call() throws Exception {
-				var millis = new Random().nextInt(1000);
-				Thread.sleep(millis);
-				var currentSpan = BookController.this.tracer.currentSpan();
-				if (currentSpan != null) {
-					currentSpan.tag("callable-sleep-millis", String.valueOf(millis));
-				}
-				return "async hi: " + currentSpan;
-			}
-		};
 	}
 	
 	@GetMapping("/book/{bookId}")
