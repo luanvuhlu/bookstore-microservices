a75eaef9753172d53c7f7964eb56cd893110eb14
luanvu <luanvuhlu@gmail.com>
2021-06-25 00:10:15 +0700

    add audit service

--- bookstore/src/main/java/com/luanvv/microservices/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/luanvv/microservices/bookstore/controller/BookController.java
@@ -1,16 +1,12 @@
 package com.luanvv.microservices.bookstore.controller;
 
-import java.io.IOException;
 import java.util.Optional;
 import java.util.Random;
 
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.luanvv.microservices.bookstore.client.AuditClient;
 import com.luanvv.microservices.bookstore.service.upload.BookImportService;
@@ -44,12 +40,6 @@ public class BookController {
 		Optional.ofNullable(this.tracer.currentSpan())
 			.ifPresent(span -> span.tag("random-sleep-millis", String.valueOf(millis)));
 		return "Book: " + bookId;
-	}
-
-	@PostMapping("/book/import")
-	public String importBooks(@RequestParam("file") MultipartFile file) throws IOException {
-		bookService.run(file.getInputStream());
-		return "Importing";
 	}
 
 }
--- bookstore/src/main/java/com/luanvv/microservices/bookstore/service/upload/BookImportService.java
+++ bookstore/src/main/java/com/luanvv/microservices/bookstore/service/upload/BookImportService.java
@@ -1,10 +1,5 @@
 package com.luanvv.microservices.bookstore.service.upload;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 public interface BookImportService {
-
-	void run(InputStream input) throws IOException;
 
 }
