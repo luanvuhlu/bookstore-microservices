9a205e896e1003fda1903c99675590abb92a4077
luanvu <luanvuhlu@gmail.com>
2022-02-28 21:36:27 +0700

    apply api specs

--- product-service/src/main/java/com/luanvv/microservices/bookstore/product/controllers/BookController.java
+++ product-service/src/main/java/com/luanvv/microservices/bookstore/product/controllers/BookController.java
@@ -1,34 +1,27 @@
 package com.luanvv.microservices.bookstore.product.controllers;
 
+import com.luanvv.bookstore.product.specs.api.ProductsApi;
+import com.luanvv.bookstore.product.specs.model.GenericResponse;
+import com.luanvv.bookstore.product.specs.model.Product;
+import com.luanvv.bookstore.product.specs.model.ProductRequest;
+import com.luanvv.bookstore.product.specs.model.ProductsList;
 import com.luanvv.microservices.bookstore.product.client.AuditClient;
 import com.luanvv.microservices.bookstore.product.client.RequestMessage;
 import com.luanvv.microservices.bookstore.product.services.AsyncService;
 import com.luanvv.microservices.bookstore.product.services.BookService;
-import com.luanvv.microservices.bookstore.product.services.request.BookRequest;
-import com.luanvv.microservices.bookstore.product.services.response.BookItemResponse;
-import com.luanvv.microservices.bookstore.product.services.response.BookResponse;
-import com.luanvv.microservices.bookstore.product.services.response.GenericResponse;
-import java.security.SecureRandom;
-import java.util.Optional;
+import com.luanvv.microservices.bookstore.product.services.response.GenericResponseUtil;
 import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.sleuth.Tracer;
-import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-public class BookController {
-
-	private final Tracer tracer;
+public class BookController implements ProductsApi {
 
 	private final AuditClient auditClient;
 
@@ -36,47 +29,52 @@ public class BookController {
 
 	private final BookService bookService;
 
-	@GetMapping("/books")
-	public Page<BookItemResponse> getBooks(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+	@Override
+	public ResponseEntity<ProductsList> getProducts(@Valid Integer page, @Valid Integer size) {
 		log.info("Get all books");
 		try {
-			return bookService.findAll(page, size);
+			return ResponseEntity.ok(bookService.findAll(page, size));
 		} finally {
 			asyncService.run(() -> auditClient.publish(new RequestMessage("Query All books")));
 		}
 	}
 
-	@GetMapping("/book/{bookId}")
-	public Optional<BookResponse> getBook(@PathVariable String bookId) throws InterruptedException {
-		var millis = new SecureRandom().nextInt(1000);
-		log.info(String.format("Sleeping for [%d] millis", millis));
-		Thread.sleep(millis);
-		Optional.ofNullable(this.tracer.currentSpan())
-				.ifPresent(span -> span.tag("random-sleep-millis", String.valueOf(millis)));
-		return bookService.findById(bookId);
+	@Override
+	public ResponseEntity<Product> getProduct(String bookId) {
+		log.info("Get product [{}]", bookId);
+		return ResponseEntity.of(bookService.findById(bookId));
 	}
 
-	@DeleteMapping("/book/{bookId}")
-	public GenericResponse deleteBook(@PathVariable String bookId) {
+	@Override
+	public ResponseEntity<GenericResponse> deleteProduct(@PathVariable String bookId) {
 		try {
 			bookService.delete(bookId);
-			return GenericResponse.ok("Delete success", bookId);
+			return ResponseEntity.ok(GenericResponseUtil.ok("Delete success", bookId));
 		} catch (EntityNotFoundException e) {
 			log.error("Cannot delete book because " + bookId + " not found", e);
-			return GenericResponse.error("Book not found", bookId);
+			return ResponseEntity.notFound().build();
 		} catch (Exception e) {
 			log.error("Delete book " + bookId + " error", e);
-			return GenericResponse.error("Delete error", bookId);
+			return ResponseEntity.internalServerError()
+					.body(GenericResponseUtil.error("Delete error", bookId));
 		}
 	}
 
-	@PostMapping("/book")
-	public GenericResponse createBook(@RequestBody BookRequest bookReq) {
+	@Override
+	public ResponseEntity<GenericResponse> putProduct(String productId,
+			@Valid ProductRequest productRequest) {
+		productRequest.setId(productId);
+		return postProduct(productRequest);
+	}
+
+	@Override
+	public ResponseEntity<GenericResponse> postProduct(@Valid ProductRequest productRequest) {
 		try {
-			return bookService.create(bookReq);
+			return ResponseEntity.ok(bookService.create(productRequest));
 		} catch (Exception e) {
 			log.error("Create book error", e);
-			return GenericResponse.error("Create error");
+			return ResponseEntity.internalServerError()
+					.body(GenericResponseUtil.error("Create error"));
 		}
 	}
 }
--- product-service/src/main/java/com/luanvv/microservices/bookstore/product/services/BookService.java
+++ product-service/src/main/java/com/luanvv/microservices/bookstore/product/services/BookService.java
@@ -1,18 +1,17 @@
 package com.luanvv.microservices.bookstore.product.services;
 
-import com.luanvv.microservices.bookstore.product.services.request.BookRequest;
-import com.luanvv.microservices.bookstore.product.services.response.BookItemResponse;
-import com.luanvv.microservices.bookstore.product.services.response.BookResponse;
-import com.luanvv.microservices.bookstore.product.services.response.GenericResponse;
+import com.luanvv.bookstore.product.specs.model.GenericResponse;
+import com.luanvv.bookstore.product.specs.model.Product;
+import com.luanvv.bookstore.product.specs.model.ProductRequest;
+import com.luanvv.bookstore.product.specs.model.ProductsList;
 import java.util.Optional;
-import org.springframework.data.domain.Page;
 
 public interface BookService {
-    Optional<BookResponse> findById(String id);
+    Optional<Product> findById(String id);
 
-    Page<BookItemResponse> findAll(int page, int size);
+    ProductsList findAll(int page, int size);
 
-    GenericResponse create(BookRequest bookReq);
+    GenericResponse create(ProductRequest bookReq);
 
     void delete(String id);
 }
