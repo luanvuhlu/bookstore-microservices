1e9e7581adb597b6c260d6db8da493d20d5f134e
luanvu <luanvuhlu@gmail.com>
2022-04-06 03:37:15 +0700

    remove LouisStoreApplicationTests

--- louis-store/src/test/kotlin/com/luanvv/microservices/bookstore/louisstore/LouisStoreApplicationTests.kt
+++ louis-store/src/test/kotlin/com/luanvv/microservices/bookstore/louisstore/LouisStoreApplicationTests.kt
@@ -1,9 +1,7 @@
 package com.luanvv.microservices.bookstore.louisstore
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class LouisStoreApplicationTests {
 
 	@Test
